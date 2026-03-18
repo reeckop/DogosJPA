bbdc4bf59589503b1d78f080e5bb807ba14df904
Ricardo Cervantes <149277067+reeckop@users.noreply.github.com>
2026-03-18 13:55:29 -0700

    Entidad Cliente

--- src/main/java/itson/dogosjpa/model/Cliente.java
+++ src/main/java/itson/dogosjpa/model/Cliente.java
@@ -2,50 +2,106 @@ package itson.dogosjpa.model;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
 
 /**
  *
  * @author Ricardo
  */
 @Entity
+@Table(name = "clientes")
 public class Cliente implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long numCliente;
 
-    public Long getId() {
-        return id;
+    @Embedded
+    private NombreCompleto nombre;
+
+    private LocalDate fchNac;
+
+    @Transient
+    private Integer edad;
+
+    @ElementCollection
+    private List<String> telefonos;
+
+    @ElementCollection
+    private List<String> preferencias;
+
+    @ManyToOne
+    @JoinColumn(name = "recomendado_por")
+    private Cliente recomienda;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Pedido> pedidos;
+
+    public Long getNumCliente() {
+        return numCliente;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setNumCliente(Long numCliente) {
+        this.numCliente = numCliente;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
+    public NombreCompleto getNombre() {
+        return nombre;
     }
 
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Cliente)) {
-            return false;
-        }
-        Cliente other = (Cliente) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
+    public void setNombre(NombreCompleto nombre) {
+        this.nombre = nombre;
     }
 
-    @Override
-    public String toString() {
-        return "itson.dogosjpa.model.Cliente[ id=" + id + " ]";
+    public LocalDate getFchNac() {
+        return fchNac;
     }
+
+    public void setFchNac(LocalDate fchNac) {
+        this.fchNac = fchNac;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public List<String> getTelefonos() {
+        return telefonos;
+    }
+
+    public void setTelefonos(List<String> telefonos) {
+        this.telefonos = telefonos;
+    }
+
+    public List<String> getPreferencias() {
+        return preferencias;
+    }
+
+    public void setPreferencias(List<String> preferencias) {
+        this.preferencias = preferencias;
+    }
+
+    public Cliente getRecomienda() {
+        return recomienda;
+    }
+
+    public void setRecomienda(Cliente recomienda) {
+        this.recomienda = recomienda;
+    }
+
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(List<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
+    
     
 }
